1f8b96d5e967754f3fe2f6239950ffda03ed7980
Matt Tilley <mtilley255@gmail.com>
2018-07-18 21:49:30 -0500

    Added products, product images, product specifications, etc.

--- index.js
+++ index.js
@@ -5,8 +5,68 @@ var app = new Vue({
     
     data: {
         
-        picked: ''
+        picked: '',
+        breaker: ''
     },
+    
+    
+    breakers: [
+        
+        { 
+            productID:          "HOM115",
+            productAmperage:    15,
+            productPoles:       "SINGLE",
+            productImage:       "ASSETS/IMAGES/HOM115.jpeg",
+            minSuggestedAWG:    14 
+        },
+        
+        
+        { 
+            productID:          "HOM120",
+            productAmperage:    20,
+            productPoles:       "SINGLE",
+            productImage:       "ASSETS/IMAGES/HOM120.jpeg",
+            minSuggestedAWG:    12 
+        },
+        
+        
+        { 
+            productID:          "HOM125",
+            productAmperage:    25,
+            productPoles:       "SINGLE",
+            productImage:       "ASSETS/IMAGES/HOM125.jpeg",
+            minSuggestedAWG:    10
+        },
+        
+        
+        { 
+            productID:          "HOM130",
+            productAmperage:    30,
+            productPoles:       "SINGLE",
+            productImage:       "ASSETS/IMAGES/HOM130.jpeg",
+            minSuggestedAWG:    10 
+        },
+        
+        
+        { 
+            productID:          "HOM140",
+            productAmperage:    40,
+            productPoles:       "SINGLE",
+            productImage:       "ASSETS/IMAGES/HOM140.jpeg",
+            minSuggestedAWG:    08 
+        },
+        
+        
+        { 
+            productID:          "HOM150",
+            productAmperage:    50,
+            productPoles:       "SINGLE",
+            productImage:       "ASSETS/IMAGES/HOM150.jpeg",
+            minSuggestedAWG:    06 
+        }
+        
+        
+    ],
     
     
     methods: {
