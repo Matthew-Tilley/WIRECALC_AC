18ae91bb2d76b12db1e55044877498bd4b826c6c
Matt Tilley <mtilley255@gmail.com>
2018-07-19 09:11:24 -0500

    Trying something / didn't work...but I know why.

--- index.js
+++ index.js
@@ -13,56 +13,56 @@ var app = new Vue({
     breakers: [
         
         { 
-            productID:          "HOM115",
+            productCode:        "HOM115",
             productAmperage:    15,
             productPoles:       "SINGLE",
             productImage:       "ASSETS/IMAGES/HOM115.jpeg",
-            minSuggestedAWG:    14 
+            minNMB_AWG:         "14 AWG" 
         },
         
         
         { 
-            productID:          "HOM120",
+            productCode:        "HOM120",
             productAmperage:    20,
             productPoles:       "SINGLE",
             productImage:       "ASSETS/IMAGES/HOM120.jpeg",
-            minSuggestedAWG:    12 
+            minNMB_AWG:         "12 AWG"
         },
         
         
         { 
-            productID:          "HOM125",
+            productCode:        "HOM125",
             productAmperage:    25,
             productPoles:       "SINGLE",
             productImage:       "ASSETS/IMAGES/HOM125.jpeg",
-            minSuggestedAWG:    10
+            minNMB_AWG:         "10 AWG"
         },
         
         
         { 
-            productID:          "HOM130",
+            productCode:        "HOM130",
             productAmperage:    30,
             productPoles:       "SINGLE",
             productImage:       "ASSETS/IMAGES/HOM130.jpeg",
-            minSuggestedAWG:    10 
+            minNMB_AWG:         "10 AWG" 
         },
         
         
         { 
-            productID:          "HOM140",
+            productCode:        "HOM140",
             productAmperage:    40,
             productPoles:       "SINGLE",
             productImage:       "ASSETS/IMAGES/HOM140.jpeg",
-            minSuggestedAWG:    08 
+            minNMB_AWG:         "08 AWG" 
         },
         
         
         { 
-            productID:          "HOM150",
+            productCode:        "HOM150",
             productAmperage:    50,
             productPoles:       "SINGLE",
             productImage:       "ASSETS/IMAGES/HOM150.jpeg",
-            minSuggestedAWG:    06 
+            minNMB_AWG:         "06 AWG" 
         }
         
         
@@ -70,6 +70,12 @@ var app = new Vue({
     
     
     methods: {
+        
+        
+        productDisplay(productImage){
+            
+            this.image=productImage;
+        }
         
         
     }
