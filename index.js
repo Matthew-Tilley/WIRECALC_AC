3ecdd44349af44302123127ee408818ee00fdc6d
Matt Tilley <mtilley255@gmail.com>
2018-07-19 09:19:58 -0500

    Added comments / moved breakers section to the data element where it belongs.

--- index.js
+++ index.js
@@ -6,75 +6,84 @@ var app = new Vue({
     data: {
         
         picked: '',
-        breaker: ''
-    },
+        
+        
+        breaker: '',
+        
+        
+        breakers: [
+        
+            { 
+                productCode:        "HOM115",
+                productAmperage:    15,
+                productPoles:       "SINGLE",
+                productImage:       "ASSETS/IMAGES/HOM115.jpeg",
+                minNMB_AWG:         "14 AWG" 
+            },
+
+
+            { 
+                productCode:        "HOM120",
+                productAmperage:    20,
+                productPoles:       "SINGLE",
+                productImage:       "ASSETS/IMAGES/HOM120.jpeg",
+                minNMB_AWG:         "12 AWG"
+            },
+
+
+            { 
+                productCode:        "HOM125",
+                productAmperage:    25,
+                productPoles:       "SINGLE",
+                productImage:       "ASSETS/IMAGES/HOM125.jpeg",
+                minNMB_AWG:         "10 AWG"
+            },
+
+
+            { 
+                productCode:        "HOM130",
+                productAmperage:    30,
+                productPoles:       "SINGLE",
+                productImage:       "ASSETS/IMAGES/HOM130.jpeg",
+                minNMB_AWG:         "10 AWG" 
+            },
+
+
+            { 
+                productCode:        "HOM140",
+                productAmperage:    40,
+                productPoles:       "SINGLE",
+                productImage:       "ASSETS/IMAGES/HOM140.jpeg",
+                minNMB_AWG:         "08 AWG" 
+            },
+
+
+            { 
+                productCode:        "HOM150",
+                productAmperage:    50,
+                productPoles:       "SINGLE",
+                productImage:       "ASSETS/IMAGES/HOM150.jpeg",
+                minNMB_AWG:         "06 AWG" 
+            }
+        
+        
+        ],  /* END OF "BREAKERS" ARRAY */
+        
+        
+        
+    },  /* END OF "DATA" SECTION */
     
-    
-    breakers: [
-        
-        { 
-            productCode:        "HOM115",
-            productAmperage:    15,
-            productPoles:       "SINGLE",
-            productImage:       "ASSETS/IMAGES/HOM115.jpeg",
-            minNMB_AWG:         "14 AWG" 
-        },
-        
-        
-        { 
-            productCode:        "HOM120",
-            productAmperage:    20,
-            productPoles:       "SINGLE",
-            productImage:       "ASSETS/IMAGES/HOM120.jpeg",
-            minNMB_AWG:         "12 AWG"
-        },
-        
-        
-        { 
-            productCode:        "HOM125",
-            productAmperage:    25,
-            productPoles:       "SINGLE",
-            productImage:       "ASSETS/IMAGES/HOM125.jpeg",
-            minNMB_AWG:         "10 AWG"
-        },
-        
-        
-        { 
-            productCode:        "HOM130",
-            productAmperage:    30,
-            productPoles:       "SINGLE",
-            productImage:       "ASSETS/IMAGES/HOM130.jpeg",
-            minNMB_AWG:         "10 AWG" 
-        },
-        
-        
-        { 
-            productCode:        "HOM140",
-            productAmperage:    40,
-            productPoles:       "SINGLE",
-            productImage:       "ASSETS/IMAGES/HOM140.jpeg",
-            minNMB_AWG:         "08 AWG" 
-        },
-        
-        
-        { 
-            productCode:        "HOM150",
-            productAmperage:    50,
-            productPoles:       "SINGLE",
-            productImage:       "ASSETS/IMAGES/HOM150.jpeg",
-            minNMB_AWG:         "06 AWG" 
-        }
-        
-        
-    ],
     
     
     methods: {
         
         
-        productDisplay(productImage){
+        productDisplay(){
             
-            this.image=productImage;
+            if (this.breaker == 15)
+                {
+                    console.log("LKJALKJAD");
+                }
         }
         
         
