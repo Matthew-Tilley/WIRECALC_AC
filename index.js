94072c4ff2a17f7f494e58c096aae2c12e0129ec
Matt Tilley <mtilley255@gmail.com>
2018-07-19 09:50:54 -0500

    Extensive notes following attempts at getting things to work as expected.

--- index.js
+++ index.js
@@ -80,9 +80,63 @@ var app = new Vue({
         
         productDisplay(breaker){
             
-            console.log(this.breaker);
+            return this.breaker;
         
         }
+        
+        
+        let x = productDisplay();
+        
+        
+        console.log(x);
+        
+        
+        /*
+        
+        Okay...here is my rationale.  I have the function (method) set up to get a value
+        from dropdown menu any time the user selects something different.  I know it's
+        working after testing it via console.log
+        
+        So, now I want to get that value "out" of the function and be able to do something
+        else with it.  Here is what I tried:
+        
+        (1) I first attempted directly assigning function (method) to a variable of "x" just
+            to test things out.  VueJS didn't like it and threw an error.
+        
+                let x = productDisplay(breaker){
+            
+                    return this.breaker;
+        
+                }
+                
+        
+        
+        (2) I next tried to pick up any values passed through the function by instantiating
+            another variable like this:
+            
+                productDisplay(breaker){
+            
+                return this.breaker;
+        
+                }
+                
+                
+                let x = productDisplay();
+            
+        Apparently VueJS didn't like that either.  Guessing maybe it doesn't want variables
+        floating around in the "methods" section perhaps?  If so, how are we supposed to 
+        pass variables to other functions (methods) and use them elsewhere in our code???
+        (Not implying VueJS is somehow the 'problem' here...only that I don't understand how
+        things are supposed to work.)
+        
+        Help...my variables are stuck and can't be extracted from functions (methods) given 
+        current knowledge!!!  :(
+        
+        
+        
+        
+        */
+        
         
     
         
