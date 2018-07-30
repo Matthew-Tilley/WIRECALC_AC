be55a86a01cee859dae9760ec4bad3d7e1c59b08
Matt Tilley <mtilley255@gmail.com>
2018-07-30 14:06:31 -0500

    Numerous changes to set up dropdown list.

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var app = new Vue({
         
         breaker: '',
 
+        amperage: '',
+
         
         
         
@@ -80,7 +82,11 @@ var app = new Vue({
     methods: {
         
         
-        
+        maximumWattage: function() {
+
+            console.log(this.amperage);
+        }
+
         
     
         
