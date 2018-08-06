ac17c50bfa7469e26652410fa7118da2fdda6587
Matt Tilley <mtilley255@gmail.com>
2018-08-06 13:04:20 -0500

    Refactored code / removed excessive whitespace.

--- index.js
+++ index.js
@@ -14,28 +14,6 @@ var app = new Vue({
             { text: 'Three', value: 'C', flurp:"zurpTRES" }
           ],
 
-
-       
-
-
-
-
-
-
-
-
-    
-
-
-
-
-        
-        
-
-
-
-       
-
               
     },  /* END OF "DATA" SECTION */
     
