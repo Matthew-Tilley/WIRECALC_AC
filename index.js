2d6684ebd942aebf55dacf31b865c1ac2ef56361
Matt Tilley <mtilley255@gmail.com>
2018-09-03 10:36:47 -0500

    Deleted out previous nonsensical code entered for testing purposes.

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ var app = new Vue({
         addToCart: function() {
 
             this.userBasket = this.userBasket.push(this.test);
-            console.log(this.userBasket + "VVVVVVVVVVVRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
+            console.log(this.userBasket);
         },
 
     },
