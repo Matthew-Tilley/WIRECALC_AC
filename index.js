a0ffa433082f4454c986b63b2bee3e4ea383f643
Matt Tilley <mtilley255@gmail.com>
2018-09-04 12:21:32 -0500

    Amazing what walking away / doing something else for awhile can do.  Figured out the problemgit add .!

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ var app = new Vue({
                     test: this.validBreaker.productCode,
             });
 
-            console.log(this.userBasket[0]);
+            console.log(this.test);
         },
 
     },
